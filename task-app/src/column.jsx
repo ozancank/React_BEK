d32ff0706913c9473e9aa38c32137b539627f8a0
OZAN CAN KOSEMEZ <ozancank@gmail.com>
2021-03-25 16:12:12 +0300

    .

---
+++ task-app/src/column.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import Task from "./task";
+import { Droppable } from "react-beautiful-dnd";
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+`;
+
+const Column = (props) => {
+  return (
+    <Container>
+      <Title>{props.column.title}</Title>
+      <Droppable droppableId={props.column.id}>
+        {(provided) => (
+          <TaskList innerRef={provided.innerRef} {...provided.droppableProps}>
+            {props.tasks.map((task, index) => (
+              <Task key={task.id} task={task} index={index} />
+            ))}
+            {provided.placeholder}
+          </TaskList>
+        )}
+      </Droppable>
+    </Container>
+  );
+};
+
+export default Column;
