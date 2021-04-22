73670b2d6884a6c297335f54e7f97be6c2edd197
Ozan Can KÖSEMEZ <39047473+ozancank@users.noreply.github.com>
2021-04-22 22:12:44 +0300

    Update fetchWeather.js

--- react-pwa/src/api/fetchWeather.js
+++ react-pwa/src/api/fetchWeather.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const URL = "https://api.openweathermap.org/data/2.5/weather";
-const API_KEY = "c45bb0b4675cd246079c73a754454472";
+const API_KEY = "xxx";
 
 export const fetchWeather = async (query) => {
   const { data } = await axios.get(URL, {
